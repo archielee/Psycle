866df69edb52736f911003eae07bcbaaa8cf7487
Oluwatoni <funmitoni2@yahoo.co.uk>
2015-05-10 06:33:27 -0400

    Added Arduino support

---
+++ red_lights_for_bike/red_lights_for_bike.ino
@@ -0,0 +1,67 @@
+/* SparkFun WS2812 Breakout Board Example
+  SparkFun Electronics
+  date: July 25, 2013
+  license: GNU GENERAL PUBLIC LICENSE
+  
+  Requires the Adafruit NeoPixel library. It's awesome, go get it.
+  https://github.com/adafruit/Adafruit_NeoPixel
+  
+  This simple example code runs three sets of animations on a group of WS2812
+  breakout boards. The more boards you link up, the better these animations
+  will look. 
+  
+  For help linking WS2812 breakouts, checkout our hookup guide:
+  https://learn.sparkfun.com/tutorials/ws2812-breakout-hookup-guide
+  
+  Before uploading the code, make sure you adjust the two defines at the
+  top of this sketch: PIN and LED_COUNT. Pin should be the Arduino pin
+  you've got connected to the first pixel's DIN pin. By default it's
+  set to Arduino pin 4. LED_COUNT should be the number of breakout boards
+  you have linked up.
+*/
+#include <Adafruit_NeoPixel.h>
+#include "WS2812_Definitions.h"
+
+#define PIN1 5
+#define PIN2 4
+#define LED_COUNT 5
+
+// Create an instance of the Adafruit_NeoPixel class called "leds".
+// That'll be what we refer to from here on...
+Adafruit_NeoPixel leds1 = Adafruit_NeoPixel(LED_COUNT, PIN1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel leds2 = Adafruit_NeoPixel(LED_COUNT, PIN2, NEO_GRB + NEO_KHZ800);
+
+void setup()
+{
+  leds1.begin();  // Call this to start up the LED strip.
+  leds2.begin();
+  clearLEDs();   // This function, defined below, turns all LEDs off...
+  leds1.show();  // ...but the LEDs don't actually update until you call this.
+  leds2.show();
+}
+
+void loop()
+{
+  redLEDs();
+}
+void clearLEDs()
+{
+  for (int i=0; i<LED_COUNT; i++)
+  {
+    leds1.setPixelColor(i, 0);
+    leds2.setPixelColor(i, 0);
+  }
+}
+
+void redLEDs()
+{
+  for (int i=0; i<LED_COUNT; i++)
+  {
+    leds1.setPixelColor(i, 255,0,0);
+    leds2.setPixelColor(i, 255,0,0);
+  }
+  leds1.show();
+  leds2.show();
+}
+
+
--- vibrator/vibrator.ino
+++ vibrator/vibrator.ino
@@ -1,40 +1,59 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BT(10, 11); 
 void setup() {
   Serial.begin(9600);
   vibratorInit();
+  // set digital pin to control as an output
+  pinMode(13, OUTPUT);
+  // set the data rate for the SoftwareSerial port
+  BT.begin(9600);
+  // Send test message to other device
+  BT.println("Hello from Arduino");
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
 }
 
-int state = 0, inByte;
+int state = LOW;
+unsigned long previousMillis = 0;
 const long interval = 500;
 
 void vibratorInit(void) {
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(2, OUTPUT);      //10 left
+  pinMode(3, OUTPUT);    //11 right
 }
+
+char a; // stores incoming character from other device
 
 void loop() {
-  bluetoothCheck();
-  
-  while(state == 1) // Right turn
-    vibrate();
-}
-
-void bluetoothCheck(void) {
-  if(Serial.available()) {
-    inByte = (int)Serial.read();
-    inByte -= 48;
-    if(inByte == 1) {
-      state = inByte;
-      Serial.println(state, DEC); // For testing purposes
+  if (BT.available())
+    // if text arrived in from BT serial...
+    {
+      a=(BT.read());
+      if (a=='L')
+      {
+        digitalWrite(13, HIGH);
+        digitalWrite(2, HIGH);
+        BT.println("LED on");
+        delay(500);
+        digitalWrite(13, LOW);
+        digitalWrite(2, LOW);
+      }
+      else if (a=='R')
+      {
+        digitalWrite(13, LOW);
+        digitalWrite(3, HIGH);
+        BT.println("LED off");
+        delay(500);
+        digitalWrite(3, LOW);
+      }
+      else if (a=='?')
+      {
+        BT.println("Send '1' to turn LED on");
+        BT.println("Send '2' to turn LED on");
+      }   
+      // you can add more "if" statements with other characters to add more commands
     }
+    
   }
-}
 
-void vibrate(void) {
-  unsigned long start = millis();
-  digitalWrite(10, HIGH);
-  digitalWrite(11, HIGH);
-  while((millis() - start) < interval) {}
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
-  state = 0;
-}
+
