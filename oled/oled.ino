87ac0788acde542c0e08bd8545dda4006c12d358
Archie Lee <archie.lee@uwaterloo.ca>
2015-05-10 13:15:09 -0400

    Uncommented barometer code

--- oled/oled.ino
+++ oled/oled.ino
@@ -1,18 +1,13 @@
 #include <Wire.h>
 #include <SeeedOLED.h>
 #include "Barometer.h"
+#include <Time.h>
 
 float temperature;
 float pressure;
+time_t t;
 
 Barometer barom;
-
-void setup() {
-  Wire.begin();
-  SeeedOled.init();
-  initOledTitle();
-  barom.init();
-}
 
 void initOledTitle(void){
   SeeedOled.clearDisplay();          //clear the screen and set start position to top left corner
@@ -24,6 +19,18 @@ void initOledTitle(void){
   SeeedOled.putString("Temp:");       //Print the String
   SeeedOled.setTextXY(3,0);
   SeeedOled.putString("Pres:");
+  SeeedOled.setTextXY(5,0);
+  SeeedOled.putString("Time:");
+  SeeedOled.setTextXY(6,0);
+  SeeedOled.putString("Date:");
+}
+
+void setup() {
+  Wire.begin();
+  SeeedOled.init();
+  initOledTitle();
+  t = now();
+  barom.init();
 }
 
 void oledMsg() {
@@ -33,11 +40,24 @@ void oledMsg() {
   SeeedOled.setTextXY(3,6);
   SeeedOled.putNumber(pressure);
   SeeedOled.putString(" Pa");
+  SeeedOled.setTextXY(5,6);
+  SeeedOled.putNumber(hour(t));
+  SeeedOled.setTextXY(5,9);
+  SeeedOled.putNumber(minute(t));
+  SeeedOled.setTextXY(5,12);
+  SeeedOled.putNumber(second(t));
+  SeeedOled.setTextXY(6,6);
+  SeeedOled.putNumber(month(t));
+  SeeedOled.setTextXY(6,9);
+  SeeedOled.putNumber(day(t));
+  SeeedOled.setTextXY(6,12);
+  SeeedOled.putNumber(year(t));
 }
 
 void loop() {
+  t = now();
   temperature = barom.bmp085GetTemperature(barom.bmp085ReadUT());
   pressure = barom.bmp085GetPressure(barom.bmp085ReadUP());
   oledMsg();
-  delay(500);
+  delay(1000);
 }
